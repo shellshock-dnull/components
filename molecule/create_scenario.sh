e37484479749a2a1f5158d776cbd91dbd22abe35
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2023-06-12 21:27:06 +0300

    create_scenario

---
+++ molecule/create_scenario.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+COMPONENT="${1}"
+
+cd $(git rev-parse --show-toplevel)/molecule
+
+cp -rf default "${COMPONENT}"
+sed -i "s/dummy/${COMPONENT}/g" "${COMPONENT}/converge.yml"
+
+cd $(git rev-parse --show-toplevel)/.github/workflows
+cp -f dummy.yml "${COMPONENT}.yml"
+sed -i "s/dummy/${COMPONENT}/g" "${COMPONENT}.yml"
