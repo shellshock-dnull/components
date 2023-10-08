643a210e5d4b3e264a549afd8c8b39f105ac2dcf
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2023-10-08 15:27:18 +0300

    create_scenario - macos fixes

--- molecule/create_scenario.sh
+++ molecule/create_scenario.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+# TODO: make this more usable
+
 
 COMPONENT="${1}"
 
 cd $(git rev-parse --show-toplevel)/molecule
 
 cp -rf default "${COMPONENT}"
-sed -i "s/dummy/${COMPONENT}/g" "${COMPONENT}/converge.yml"
+if [[ "$(uname -o)" == "Darwin" ]]; then
+  sed -i '' "s/dummy/${COMPONENT}/g" "${COMPONENT}/converge.yml"
+else
+  sed -i "s/dummy/${COMPONENT}/g" "${COMPONENT}/converge.yml"
+fi
 
 cd $(git rev-parse --show-toplevel)/.github/workflows
 cp -f ../dummy.yml "${COMPONENT}.yml"
-sed -i "s/dummy/${COMPONENT}/g" "${COMPONENT}.yml"
+if [[ "$(uname -o)" == "Darwin" ]]; then
+  sed -i '' "s/dummy/${COMPONENT}/g" "${COMPONENT}.yml"
+else
+  sed -i "s/dummy/${COMPONENT}/g" "${COMPONENT}.yml"
+fi
